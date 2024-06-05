07d1830635a22ebeefe15c87ac81b4a802de48fa
MuhamadReyvanathaHernawan <148271256+MuhamadReyvanathaHernawan@users.noreply.github.com>
2024-06-05 12:46:11 +0700

    membuat beberapa fitur

--- haloPetani.sql
+++ haloPetani.sql
@@ -3,7 +3,7 @@ CREATE TABLE admin (
     id_admin INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nama_admin VARCHAR(260) NOT NULL,
     password_admin VARCHAR(260) NOT NULL    
-)
+);
 
 CREATE TABLE artikel (
 
@@ -13,7 +13,7 @@ CREATE TABLE artikel (
     kategori_artikel VARCHAR(300) NOT NULL,
     tanggal_artikel DATE NOT NULL,
     rating FLOAT
-)
+);
 
 CREATE TABLE pengguna (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -21,9 +21,9 @@ CREATE TABLE pengguna (
     no_hp VARCHAR(260) NOT NULL,
     password VARCHAR(260) NOT NULL,
     quality_point INT 
-)
+);
 
 CREATE TABLE tanaman (
     id_tanaman INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     jenis_tanaman VARCHAR(260) NOT NULL 
-)
+);
